fc0530c046059a14bbfd44438b35becc82e17abd
Vernon Lillies <vernon.lillies@gmail.com>
2017-10-16 23:54:38 -0400

    add mlab database

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
 var mongoose = require('mongoose');
 mongoose.set('debug',true);
-mongoose.connect('mongodb://localhost/todo-api', {useMongoClient: true}); //create database if does nto exit or connect
+
+// mongoose.connect('mongodb://localhost/todo-api', {useMongoClient: true}); //create database if does nto exit or connect
+
+mongoose.connect('mongodb://vernon:cows56m5@ds121945.mlab.com:21945/todos-api', {useMongoClient: true});
+
 
 mongoose.Promise = Promise; //allow us to use the promise syntax
 
 module.exports.Todo = require("./todo")
+
